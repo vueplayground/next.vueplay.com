63ebac701bfb4a14b1c0d661a71f5abf519b613c
steffanhalv <steffanhalv@gmail.com>
2023-06-16 11:40:49 +0000

    Build: (697279e) Smaller ctx icon

--- assets/javascript-ffb8d3ac.js
+++
@@ -1,6 +0,0 @@
-import{conf as t,language as e}from"./typescript-00daf1a3.js";import"./Design-ec09b099.js";import"./SectionRenderer-7b88dc6b.js";import"./compiler-f3733640.js";import"./index-7d95dacf.js";import"./ModalProfileSubscriptionPricing-a92340d5.js";/*!-----------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.39.0(ff3621a3fa6389873be5412d17554294ea1b0941)
- * Released under the MIT license
- * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
- *-----------------------------------------------------------------------------*/var c=t,l={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{c as conf,l as language};
