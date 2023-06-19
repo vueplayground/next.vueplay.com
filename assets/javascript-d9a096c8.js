5582a215a84d8e834a9944ff92ebc2196c4df703
steffanhalv <steffanhalv@gmail.com>
2023-06-19 20:26:34 +0000

    Build: (0d16091) Remove parent color

--- assets/javascript-d9a096c8.js
+++
@@ -1,6 +0,0 @@
-import{conf as t,language as e}from"./typescript-2f327768.js";import"./Design-7c5652fe.js";import"./SectionRenderer-6c2536a4.js";import"./compiler-19dd5a6d.js";import"./index-abd15228.js";import"./ModalProfileSubscriptionPricing-68a34e60.js";/*!-----------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.39.0(ff3621a3fa6389873be5412d17554294ea1b0941)
- * Released under the MIT license
- * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
- *-----------------------------------------------------------------------------*/var c=t,l={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{c as conf,l as language};
