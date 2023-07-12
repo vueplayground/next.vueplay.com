7bc75c1a54c16dec500fad20e052103930ef22c1
steffanhalv <steffanhalv@gmail.com>
2023-07-12 18:46:43 +0000

    Build: (edb4c88) Fix create new app

---
+++ assets/javascript-516bd746.js
@@ -0,0 +1,6 @@
+import{conf as t,language as e}from"./typescript-28111e54.js";import"./Design-00164212.js";import"./SectionRenderer-25344cd1.js";import"./compiler-dce09a00.js";import"./index-ee3db2f1.js";import"./ModalProfileSubscriptionPricing-ca441c24.js";/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.39.0(ff3621a3fa6389873be5412d17554294ea1b0941)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/var c=t,l={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{c as conf,l as language};
